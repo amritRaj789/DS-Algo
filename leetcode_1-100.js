555349b7eae18588de2835af72783f7e9b6d8652
Amrit Raj <amritrazzz@gmail.com>
2021-02-21 11:08:32 +0530

    Problem 15

--- leetcode_1-100.js
+++ leetcode_1-100.js
@@ -82,6 +82,38 @@ function maxWater (height){
 }
 
 
+/*15. 3Sum
+Given an array nums of n integers, are there elements a, b, c in nums such that a + b + c = 0? Find all unique triplets in the array which gives the sum of zero.
+Notice that the solution set must not contain duplicate triplets.
+*/
+var threeSum = function (nums){
+	nums.sort((a, b) => a-b);
+	let result = [];
+	for(let i = 0; i < nums.length-2; i++){
+		if(i > 0 && nums[i] === nums[i-1])
+			continue;
+		twoSum(nums, i+1, -nums[i], result);
+	}
+	return result;
+}
+function twoSum (arr, left, target, result){
+	let right = arr.length-1;
+	while(left < right){
+		if(arr[left] + arr[right] < target)
+			left++;
+		else if(arr[left] + arr[right] > target)
+			right--;
+		else{
+			result.push([-target, arr[left], arr[right]]);
+			left++;
+			right--;
+			while(left < right && arr[left] === arr[left-1])
+				left++
+			while(left < right && arr[right] === arr[right+1])
+				right--;
+		}
+	}
+}
 
 
 
