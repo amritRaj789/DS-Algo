ed5c7c957deafcce2c73521ab9e3d82b1f5579ed
amritRaj789 <amritrazzz@gmail.com>
2021-04-12 20:53:31 +0530

    --96. Unique Binary Search trees

--- leetcode_1-100.js
+++ leetcode_1-100.js
@@ -432,3 +432,23 @@ function deleteDuplicates (head){
 }
 
 //Hello from my new Laptop
+
+/*96. Unique Binary Search Trees
+Given an integer n, return the number of structurally unique BST's (binary search trees) 
+which has exactly n nodes of unique values from 1 to n.*/
+
+#red red red red red red red red red red red red
+
+var numTrees = function(n) {
+    let G = new Array(n+1).fill(0);
+    G[0] = 1;
+    G[1] = 1;
+    for (let i=2;i<=n;i++) {
+        for (let j=1;j<=i;j++) {
+            G[i]+=G[j-1] * G[i - j];
+        }
+    }
+    return G[n];
+};
+// We just have to arrange different no. of nodes on each side of the root node
+// The value of the nodes doesn't matter. Suppose we select 4 as root node, then 1 to 3 goes to left and 5 to n goes to right
