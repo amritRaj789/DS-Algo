ddda519e9effb058136ff47b205a8a1845a91c0f
amritRaj789 <amritrazzz@gmail.com>
2021-04-19 22:27:04 +0530

    --334 Increasing Triplet Subsequence

--- leetcode_300_400.js
+++ leetcode_300_400.js
@@ -13,3 +13,25 @@ var removeDuplicateLetters = function(s) {
 console.log(removeDuplicateLetters("bcabc"));
 // Incomplete
 
+334. Increasing Triplet Subsequence
+
+/*Given an integer array nums, return true if there exists a triple of indices (i, j, k) such that i < j < k and nums[i] < nums[j] < nums[k]. 
+If no such indices exists, return false.*/
+
+// yay I did it, I came up with O(N) t and O(1) s solution
+let increasingTriplet = function (nums){
+	if(nums.length < 3)
+		return false;
+	let minTillNow = nums[0];
+	let secMinTillNow = +Infinity;
+	for(let i = 1; i < nums.length; i++){
+		if(nums[i] > minTillNow){
+			if(nums[i] > secMinTillNow)
+				return true;
+			secMinTillNow = Math.min(secMinTillNow, nums[i])
+		}
+		else
+			minTillNow = nums[i];
+	}
+	return false;	
+}
