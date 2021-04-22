e5da89826865261348f092baf826ecdfe0173ecb
amritRaj789 <amritrazzz@gmail.com>
2021-04-22 11:29:17 +0530

    --9. Palindrome Number

--- leetcode_1-100.js
+++ leetcode_1-100.js
@@ -91,6 +91,24 @@ var convert = function(s, numRows) {
     return answer;
 };
 
+/*9. Palindrome Number
+Given an integer x, return true if x is palindrome integer.
+An integer is a palindrome when it reads the same backward as forward. 
+For example, 121 is palindrome while 123 is not.
+*/
+var isPalindrome = function(x) {
+    if(x < 0)
+        return false;
+    let original = x;
+    let reversed = 0;
+    while(x > 0){
+        reversed = reversed*10 + (x%10);
+        x = Math.floor(x/10);
+    }
+    return original === reversed
+};
+ 
+
 // 11. Container with Most Water
 /*Given n non-negative integers a1, a2, ..., an , where each represents a point at coordinate (i, ai). 
 n vertical lines are drawn such that the two endpoints of the line i is at (i, ai) and (i, 0). Find two lines, which, together with the x-axis forms a container, such that the container contains the most water.
