987f7a38d6dd1ad7f4cffc220712df2390c9fc6f
amritRaj789 <amritrazzz@gmail.com>
2021-04-23 21:37:02 +0530

    --39. Combination Sum

--- leetcode_1-100.js
+++ leetcode_1-100.js
@@ -453,6 +453,35 @@ let solveSudoku = function (board){
     backTrack(0);
     return board;
 }
+
+//39. Combination Sum
+
+/*Given an array of distinct integers candidates and a target integer target, 
+return a list of all unique combinations of candidates where the chosen numbers sum to target. You may return the combinations in any order.
+The same number may be chosen from candidates an unlimited number of times. 
+Two combinations are unique if the frequency of at least one of the chosen numbers is different.
+It is guaranteed that the number of unique combinations that sum up to target 
+is less than 150 combinations for the given input.
+*/
+var combinationSum = function(candidates, target) {
+  let result = [];
+    function recursive(sum, arr, i){
+      if(sum === target){
+        result.push([...arr]);
+        return
+      }
+      if(i === candidates.length)
+        return 
+      if(candidates[i]+sum > target)
+        recursive(sum, [...arr], i+1);
+      else{
+        recursive(sum+candidates[i], [...arr, candidates[i]], i)
+        recursive(sum, [...arr], i+1);
+      }
+    }
+  recursive(0, [], 0);
+  return result;
+};
 /*51. N-Queens
 Return all distinct solutions to the n queens problem
 */
