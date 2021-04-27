c16a810bf156319da205a19c201092300f2dc1ee
amritRaj789 <amritrazzz@gmail.com>
2021-04-27 09:48:47 +0530

    --526. Beautiful Arrangement

--- leetcode_500-600.js
+++ leetcode_500-600.js
@@ -75,3 +75,28 @@ function findAnagrams (s, p){
 
 
 
+// 526. Beautiful Arrangement
+/*Suppose you have n integers labeled 1 through n. A permutation of those n integers perm (1-indexed) is considered a beautiful arrangement if for every i (1 <= i <= n), either of the following is true:
+perm[i] is divisible by i.
+i is divisible by perm[i].
+Given an integer n, return the number of the beautiful arrangements that you can construct.
+*/
+let countArrangement = function (n) {
+	let count = 0;
+	let visited = Array(n+1).fill(true);
+	function recursive(level){
+		if(level === n+1){
+			count++;
+			return
+		}
+		for(let i = 1; i <= n; i++){
+			if(visited[i] === true && (i%level === 0 || level%i === 0)){
+				visited[i] = false;
+				recursive(level+1);
+				visited[i] = true;
+			}
+		}
+	}
+	recursive(1)
+	return count;	
+}
