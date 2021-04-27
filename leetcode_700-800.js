d2f682f2fd2ee4e8da59acf6c9168081052cf9f5
amritRaj789 <amritrazzz@gmail.com>
2021-04-27 12:10:59 +0530

    --797. All paths from source to target

--- leetcode_700-800.js
+++ leetcode_700-800.js
@@ -34,3 +34,25 @@ let letterCasePermutation = function(S) {
     recursive(0, s);
     return result;
 };
+
+/*797. All Paths From Source to Target
+
+Given a directed acyclic graph (DAG) of n nodes labeled from 0 to n - 1, find all possible paths 
+from node 0 to node n - 1, and return them in any order.
+The graph is given as follows: graph[i] is a list of all nodes you can visit from node i 
+(i.e., there is a directed edge from node i to node graph[i][j]).
+*/
+var allPathsSourceTarget = function(graph) {
+    let result = [];
+    function recursive(index, arr){
+        if(index === graph.length-1){
+            result.push([...arr, index])
+            return
+        }
+        for(let i = 0; i < graph[index].length; i++){
+            recursive(graph[index][i], [...arr, index]);
+        }
+    }
+    recursive(0, []);
+    return result;
+};
