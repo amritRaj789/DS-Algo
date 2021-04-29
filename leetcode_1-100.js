7e6beeb996aa3c6ccaac03b42924de2558701f53
amritRaj789 <amritrazzz@gmail.com>
2021-04-29 20:26:39 +0530

    --33. Search in Rotated Array

--- leetcode_1-100.js
+++ leetcode_1-100.js
@@ -367,6 +367,33 @@ var swapPairs = function (head){
 	return head;
 }
 
+/*33. Search in Rotated Array
+
+Given the array nums after the rotation and an integer target, 
+return the index of target if it is in nums, or -1 if it is not in nums.
+*/
+var search = function(nums, target) {
+    let left = 0;
+    let right = nums.length-1;
+    while(left <= right){
+        mid = Math.floor((left+right)/2);
+        if(target === nums[mid])
+            return mid;
+        if(nums[left] <= nums[mid]){
+            if(target >= nums[left] && target < nums[mid])
+                right = mid-1;
+            else
+                left = mid+1;
+        }
+        else{
+            if(target > nums[mid] && target <= nums[right])
+                left = mid+1;
+            else
+                right = mid-1;
+        }
+    }
+    return -1;    
+};
 
 /*34. Find first and last position of element in Sorted Array
 */
