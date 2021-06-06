1ff4a249ece4bcc3d6ab217efd6c2e35163e26ab
amritRaj789 <amritrazzz@gmail.com>
2021-06-06 18:42:32 +0530

    --1828. Queries on Number of Points inside a circle

--- leetcode_1800-1900.js
+++ leetcode_1800-1900.js
@@ -70,6 +70,30 @@ let minSideJumps = function (obstacle) {
 
 // I feel so good after solving this.
 
+// 1828. Queries on Number of Points inside a Circle
+
+/* You are given an array points where points[i] = [xi, yi] is the coordinates of the ith point on a 2D plane. Multiple points can have the same coordinates.
+You are also given an array queries where queries[j] = [xj, yj, rj] describes a circle centered at (xj, yj) with a radius of rj.
+For each query queries[j], compute the number of points inside the jth circle. Points on the border of the circle are considered inside.
+Return an array answer, where answer[j] is the answer to the jth query. */
+
+var countPoints = function (points, queries) {
+  let result = [];
+  for (let i = 0; i < queries.length; i++) {
+    let count = 0;
+    for (let j = 0; j < points.length; j++) {
+      count +=
+        (points[j][0] - queries[i][0]) ** 2 +
+          (points[j][1] - queries[i][1]) ** 2 <=
+        queries[i][2] ** 2
+          ? 1
+          : 0;
+    }
+    result.push(count);
+  }
+  return result;
+};
+
 /* 
 1861. Rotating the Box
 
