f8f34bac7c4fedf6e073cc7cdec662b00c08c7a9
amritRaj789 <amritrazzz@gmail.com>
2021-06-13 14:43:03 +0530

    --171. Excel Sheet Column Number

--- leetcode_100-200.js
+++ leetcode_100-200.js
@@ -813,6 +813,20 @@ var convertToTitle = function (columnNumber) {
   return result;
 };
 
+/* 171. Excel Sheet Column Number
+
+Given a string columnTitle that represents the column title as appear in an Excel sheet, return its corresponding column number.
+ */
+var titleToNumber = function (columnTitle) {
+  let sum = 0;
+  for (let i = columnTitle.length - 1; i >= 0; i--) {
+    sum +=
+      Math.pow(26, columnTitle.length - 1 - i) *
+      (columnTitle.charCodeAt(i) - 64);
+  }
+  return sum;
+};
+
 //200. Number of Islands
 /*Given an m x n 2D binary grid grid which represents a map of '1's (land) and '0's (water), return the number of islands.
 An island is surrounded by water and is formed by connecting adjacent lands horizontally or vertically. You may assume all 
