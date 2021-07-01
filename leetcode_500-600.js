93dcc6a68631b12309304175cb15233ead00f8a5
amritRaj789 <amritrazzz@gmail.com>
2021-07-01 13:08:40 +0530

    --563. Binary Tree Tilt

--- leetcode_500-600.js
+++ leetcode_500-600.js
@@ -252,3 +252,22 @@ var maxDepth = function (root) {
   }
   return dfs(root);
 };
+
+/* 563. Binary Tree Tilt
+
+Given the root of a binary tree, return the sum of every tree node's tilt.
+The tilt of a tree node is the absolute difference between the sum of all left subtree node values and all right subtree node values. If a node does not have a left child, then the sum of the left subtree node values is treated as 0. The rule is similar if there the node does not have a right child.
+ */
+
+var findTilt = function (root) {
+  if (!root) return 0;
+  let sum = 0;
+  function dfs(node) {
+    let left = node.left ? dfs(node.left) : 0;
+    let right = node.right ? dfs(node.right) : 0;
+    sum += Math.abs(left - right);
+    return left + right + node.val;
+  }
+  dfs(root);
+  return sum;
+};
