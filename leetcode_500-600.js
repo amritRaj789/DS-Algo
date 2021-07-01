c9384481b4f3747298e5c9973948f6b38e897721
amritRaj789 <amritrazzz@gmail.com>
2021-07-01 12:44:31 +0530

    --543. Diameter of a Binary Tree

--- leetcode_500-600.js
+++ leetcode_500-600.js
@@ -214,3 +214,22 @@ let convertBST = function (root) {
   postOrder(root);
   return root;
 };
+
+/* 543. Diameter of a Binary Tree
+
+Given the root of a binary tree, return the length of the diameter of the tree.
+The diameter of a binary tree is the length of the longest path between any two nodes in a tree. This path may or may not pass through the root.
+The length of a path between two nodes is represented by the number of edges between them.
+ */
+var diameterOfBinaryTree = function (root) {
+  let diameter = 0;
+  function dfs(node) {
+    if (!node) return 0;
+    let left = dfs(node.left);
+    let right = dfs(node.right);
+    diameter = Math.max(left + right + 1, diameter);
+    return Math.max(left, right) + 1;
+  }
+  dfs(root);
+  return diameter - 1;
+};
