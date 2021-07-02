f5116da7d796d863765b9a3b2f31d07a985b26fc
amritRaj789 <amritrazzz@gmail.com>
2021-07-02 22:49:02 +0530

    --700. Search in a Binary Search Tree

--- leetcode_600-700.js
+++ leetcode_600-700.js
@@ -136,3 +136,16 @@ let findSecondMinimumValue = function (root) {
   dfs(root);
   return secondMin === Infinity ? -1 : secondMin;
 };
+
+/* 700. Search in a BST
+
+You are given the root of a binary search tree (BST) and an integer val.
+Find the node in the BST that the node's value equals val and return the subtree rooted with that node. If such a node does not exist, return null.
+ */
+
+let searchBST = function (root, val) {
+  if (!root) return null;
+  if (root.val === val) return root;
+  else if (root.val < val) return searchBST(root.right, val);
+  else return searchBST(root.left, val);
+};
