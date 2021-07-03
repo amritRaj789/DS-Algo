6b19482b84c622c509d1b761f972700d856a3b39
amritRaj789 <amritrazzz@gmail.com>
2021-07-03 22:51:45 +0530

    --1189. Maximum Number of Balloons

--- leetcode_1100-1200.js
+++ leetcode_1100-1200.js
@@ -13,3 +13,32 @@ let longestCommonSubsequence = function(text1, text2){
     }
     return dp[text1.length][text2.length];
 }
+
+/* 1189. Maximum Number of Balloons
+
+Given a string text, you want to use the characters of text to form as many instances of the word "balloon" as possible.
+You can use each character in text at most once. Return the maximum number of instances that can be formed.
+ */
+ 
+let maxNumberOfBalloons = function (text) {
+  let hash = {
+    b: 0,
+    a: 0,
+    l: 0,
+    o: 0,
+    n: 0,
+  };
+  for (let char of text) {
+    if (char in hash) hash[char]++;
+  }
+  hash["b"] *= 2;
+  hash["a"] *= 2;
+  hash["n"] *= 2;
+  let min = +Infinity;
+  for (let key in hash) {
+    min = Math.min(hash[key], min);
+  }
+  if (min === Infinity) return 0;
+  if (min % 2) return (min - 1) / 2;
+  return min / 2;
+};
