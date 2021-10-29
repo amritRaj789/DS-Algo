662fc2bea1b1aa44e89459c025618a043438c97f
amritRaj789 <amritrazzz@gmail.com>
2021-10-29 12:18:42 -0700

    --48. Rotate Image

--- leetcode_1-100.js
+++ leetcode_1-100.js
@@ -760,6 +760,28 @@ let permuteUnique = function (nums) {
   return result;
 };
 
+/* 48. Rotate Image
+
+You are given an n x n 2D matrix representing an image, rotate the image by 90 degrees (clockwise).
+You have to rotate the image in-place, which means you have to modify the input 2D matrix directly. DO NOT allocate another 2D matrix and do the rotation.
+ */
+
+var rotate = function (matrix) {
+  let n = matrix.length;
+  let depth = Math.floor(n / 2);
+  for (let i = 0; i < depth; i++) {
+    let temp;
+    let opp = n - i - 1;
+    for (let j = 0; j < n - 2 * i - 1; j++) {
+      temp = matrix[i][i + j];
+      matrix[i][i + j] = matrix[opp - j][i];
+      matrix[opp - j][i] = matrix[opp][opp - j];
+      matrix[opp][opp - j] = matrix[i + j][opp];
+      matrix[i + j][opp] = temp;
+    }
+  }
+};
+
 /* 49. Group Anagrams
 
 Given an array of strings strs, group the anagrams together. You can return the answer in any order.
