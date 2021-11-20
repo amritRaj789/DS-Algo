f101afc58f3e201c4539e2d9c6711df4aae1a56f
amritRaj789 <amritrazzz@gmail.com>
2021-11-20 14:59:12 -0800

    --830. Positions of Large Groups

--- leetcode_800-900.js
+++ leetcode_800-900.js
@@ -117,6 +117,30 @@ var toGoatLatin = function (sentence) {
   return result;
 };
 
+//830. Positions of Large Groups
+
+/* In a string s of lowercase letters, these letters form consecutive groups of the same character.
+For example, a string like s = "abbxxxxzyy" has the groups "a", "bb", "xxxx", "z", and "yy".
+A group is identified by an interval [start, end], where start and end denote the start and end indices (inclusive) of the group. In the above example, "xxxx" has the interval [3,6].
+A group is considered large if it has 3 or more characters.
+Return the intervals of every large group sorted in increasing order by start index.
+ */
+
+var largeGroupPositions = function (s) {
+  let result = [];
+  let start = 0;
+  for (let i = 0; i < s.length - 1; i++) {
+    if (s[i] !== s[i + 1]) {
+      if (i - start >= 2) {
+        result.push([start, i]);
+      }
+      start = i + 1;
+    }
+  }
+  if (s.length - start >= 3) result.push([start, s.length - 1]);
+  return result;
+};
+
 /*842. Split Array into Fibonacci Sequence
 
 Given a string S of digits, such as S = "123456579", we can split it into a Fibonacci-like sequence [123, 456, 579].
