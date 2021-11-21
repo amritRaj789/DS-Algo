40d3400d5576d2bb07990e91e88a016345a0f7e5
amritRaj789 <amritrazzz@gmail.com>
2021-11-21 09:56:45 -0800

    --836. Rectangle Overlap

--- leetcode_800-900.js
+++ leetcode_800-900.js
@@ -160,6 +160,37 @@ let flipAndInvertImage = function (image) {
   return result;
 };
 
+//836. Rectangle Overlap
+
+/* An axis-aligned rectangle is represented as a list [x1, y1, x2, y2], where (x1, y1) is the coordinate of its bottom-left corner, and (x2, y2) is the coordinate of its top-right corner. Its top and bottom edges are parallel to the X-axis, and its left and right edges are parallel to the Y-axis.
+Two rectangles overlap if the area of their intersection is positive. To be clear, two rectangles that only touch at the corner or edges do not overlap.
+Given two axis-aligned rectangles rec1 and rec2, return true if they overlap, otherwise return false.
+ */
+let isRectangleOverlap = function (rec1, rec2) {
+  // take reference rectangle as R1
+  // Case 1: Rec2 is to the left of R1
+  if (rec2[2] <= rec1[0]) return false;
+  // Case 2: Rec2 is to the right of R1
+  else if (rec2[0] >= rec1[2]) return false;
+  // Case 3 : Rec2 is to the top of R1
+  else if (rec2[1] >= rec1[3]) return false;
+  // Case 4 : Rec2 is to the bottom of R1
+  else if (rec2[3] <= rec1[1]) return false;
+  return true;
+};
+
+// Same code, shorter version
+let isRectangleOverlap = function (rec1, rec2) {
+  if (
+    rec2[2] <= rec1[0] ||
+    rec2[0] >= rec1[2] ||
+    rec2[1] >= rec1[3] ||
+    rec2[3] <= rec1[1]
+  )
+    return false;
+  return true;
+};
+
 /*842. Split Array into Fibonacci Sequence
 
 Given a string S of digits, such as S = "123456579", we can split it into a Fibonacci-like sequence [123, 456, 579].
