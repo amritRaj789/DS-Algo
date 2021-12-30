12a7b1c2008a1d9518d89f6fea2f8e26c74a9493
amritRaj789 <amritrazzz@gmail.com>
2021-12-30 13:59:04 -0800

    --1047. Remove all Duplicates in String

--- leetcode_1000-1100.js
+++ leetcode_1000-1100.js
@@ -16,6 +16,29 @@ var bstToGst = function (root) {
   return root;
 };
 
+/* 1047. Remove all Duplicates in String
+
+You are given a string s consisting of lowercase English letters. A duplicate removal consists of choosing two adjacent and equal letters and removing them.
+We repeatedly make duplicate removals on s until we no longer can.
+Return the final string after all such duplicate removals have been made. It can be proven that the answer is unique.
+ */
+
+var removeDuplicates = function (s) {
+  let stack = [];
+  for (let i = 0; i < s.length; i++) {
+    stack.push(s[i]);
+    if (stack.length > 1) {
+      if (stack[stack.length - 1] == stack[stack.length - 2]) {
+        stack.pop();
+        stack.pop();
+      }
+    }
+  }
+  let result = "";
+  stack.forEach((char) => (result += char));
+  return result;
+};
+
 /*1079. Letter Tile Possibilities
 
 You have n  tiles, where each tile has one letter tiles[i] printed on it.
