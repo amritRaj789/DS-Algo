5b481a80741c9a584c5fcc72c5df92e1ca577190
amritRaj789 <amritrazzz@gmail.com>
2022-01-08 12:05:34 -0800

    --507. Perfect Number

--- leetcode_500-600.js
+++ leetcode_500-600.js
@@ -153,6 +153,23 @@ let updateMatrix = function (mat) {
 };
 // BFS approach
 
+/* 507. Perfect Number
+
+A perfect number is a positive integer that is equal to the sum of its positive divisors, excluding the number itself. A divisor of an integer x is an integer that can divide x evenly.
+Given an integer n, return true if n is a perfect number, otherwise return false.
+ */
+var checkPerfectNumber = function (num) {
+  if (num <= 5) return false;
+  let sum = 0;
+  for (let i = 2; i < Math.sqrt(num); i++) {
+    if (num % i == 0) {
+      sum += i + num / i;
+    }
+  }
+  sum += 1;
+  return sum == num;
+};
+
 /* 513. Find Bottom Left Tree Value
 
 Given the root of a binary tree, return the leftmost value in the last row of the tree.
