a9cb5cda94121fe972a6d10b0b14b641993f2d2e
amritRaj789 <amritrazzz@gmail.com>
2022-01-08 18:13:38 -0800

    --695. Max Area of Island

--- leetcode_600-700.js
+++ leetcode_600-700.js
@@ -212,6 +212,58 @@ var hasAlternatingBits = function (n) {
   return true;
 };
 
+/* 695. Max Area of Island
+
+You are given an m x n binary matrix grid. An island is a group of 1's (representing land) connected 4-directionally (horizontal or vertical.) You may assume all four edges of the grid are surrounded by water.
+The area of an island is the number of cells with a value 1 in the island.
+Return the maximum area of an island in grid. If there is no island, return 0.
+ */
+
+var maxAreaOfIsland = function (grid) {
+  let max = 0;
+  let row = grid.length;
+  let col = grid[0].length;
+  for (let i = 0; i < row; i++) {
+    for (let j = 0; j < col; j++) {
+      if (grid[i][j] == 1) max = Math.max(max, bfs(i, j));
+    }
+  }
+  function bfs(sr, sc) {
+    let queue = [[sr, sc]];
+    let area = 1;
+    grid[sr][sc] = 0;
+    while (queue.length) {
+      let [r, c] = queue.shift();
+      // top
+      if (r != 0 && grid[r - 1][c] != 0) {
+        area++;
+        grid[r - 1][c] = 0;
+        queue.push([r - 1, c]);
+      }
+      // bottom
+      if (r != row - 1 && grid[r + 1][c] != 0) {
+        area++;
+        grid[r + 1][c] = 0;
+        queue.push([r + 1, c]);
+      }
+      // left
+      if (c != 0 && grid[r][c - 1] != 0) {
+        area++;
+        grid[r][c - 1] = 0;
+        queue.push([r, c - 1]);
+      }
+      // right
+      if (c != col - 1 && grid[r][c + 1] != 0) {
+        area++;
+        grid[r][c + 1] = 0;
+        queue.push([r, c + 1]);
+      }
+    }
+    return area;
+  }
+  return max;
+};
+
 /* 700. Search in a BST
 
 You are given the root of a binary search tree (BST) and an integer val.
