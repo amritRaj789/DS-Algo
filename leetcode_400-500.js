741ce8e5bb4b95ad14b0153238d15ca403eb9f50
amritRaj789 <amritrazzz@gmail.com>
2022-01-08 16:17:23 -0800

    --476. Number Complement

--- leetcode_400-500.js
+++ leetcode_400-500.js
@@ -486,6 +486,22 @@ var makesquare = function (matchsticks) {
   return backTrack(0, 0, 0, 0, 0);
 };
 
+/* 476. Number Complement
+
+The complement of an integer is the integer you get when you flip all the 0's to 1's and all the 1's to 0's in its binary representation.
+For example, The integer 5 is "101" in binary and its complement is "010" which is the integer 2.
+Given an integer num, return its complement.
+ */
+var findComplement = function (num) {
+  let bits = 0;
+  let n = num;
+  while (n > 0) {
+    n = n >> 1;
+    bits++;
+  }
+  return num ^ (2 ** bits - 1);
+};
+
 /* 491. Increasing Subsequences
 
 Given an integer array nums, return all the different possible increasing subsequences of the given array with at least two elements. You may return the answer in any order.
