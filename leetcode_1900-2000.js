a1404b99e724144bdad5cb8c5fe2539e299a33d9
amritRaj789 <amritrazzz@gmail.com>
2022-01-11 14:58:17 -0800

    --1991. Find the Middle Index in Array

--- leetcode_1900-2000.js
+++ leetcode_1900-2000.js
@@ -61,3 +61,21 @@ var rotateGrid = function (grid, k) {
 
   return grid;
 };
+
+/* 1991. Find the Middle Index in Array
+
+Given a 0-indexed integer array nums, find the leftmost middleIndex (i.e., the smallest amongst all the possible ones).
+A middleIndex is an index where nums[0] + nums[1] + ... + nums[middleIndex-1] == nums[middleIndex+1] + nums[middleIndex+2] + ... + nums[nums.length-1].
+If middleIndex == 0, the left side sum is considered to be 0. Similarly, if middleIndex == nums.length - 1, the right side sum is considered to be 0.
+Return the leftmost middleIndex that satisfies the condition, or -1 if there is no such index.
+ */
+
+var findMiddleIndex = function (nums) {
+  let total = nums.reduce((a, b) => a + b);
+  let leftSum = 0;
+  for (let i = 0; i < nums.length; i++) {
+    if (total - leftSum - nums[i] == leftSum) return i;
+    leftSum += nums[i];
+  }
+  return -1;
+};
