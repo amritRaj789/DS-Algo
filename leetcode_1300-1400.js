00cd4cc9bf2d5091ed54002365982413ff673e04
amritRaj789 <amritrazzz@gmail.com>
2022-01-15 07:54:32 +0530

    --1382. Balance a Binary Search Tree

--- leetcode_1300-1400.js
+++ leetcode_1300-1400.js
@@ -40,3 +40,27 @@ var minSetSize = function (arr) {
     count++;
   }
 };
+
+/* 1382. Balance a Binary Search Tree
+
+Given the root of a binary search tree, return a balanced binary search tree with the same node values. If there is more than one answer, return any of them.
+A binary search tree is balanced if the depth of the two subtrees of every node never differs by more than 1. */
+
+var balanceBST = function (root) {
+  let array = [];
+  function inOrder(node) {
+    if (node.left) inOrder(node.left);
+    array.push(node.val);
+    if (node.right) inOrder(node.right);
+  }
+  inOrder(root);
+  function recursive(arr) {
+    if (arr.length == 0) return null;
+    let mid = ~~(arr.length / 2);
+    let newRoot = new TreeNode(arr[mid]);
+    newRoot.left = recursive(arr.slice(0, mid));
+    newRoot.right = recursive(arr.slice(mid + 1));
+    return newRoot;
+  }
+  return recursive(array);
+};
