cf5947a22c119acb27654abfbfd52789a75f5f6f
amritRaj789 <amritrazzz@gmail.com>
2022-01-20 21:42:09 +0530

    --1046. Last Stone Weight

--- leetcode_1000-1100.js
+++ leetcode_1000-1100.js
@@ -16,6 +16,29 @@ var bstToGst = function (root) {
   return root;
 };
 
+/* 1046. Last Stone Weight
+
+You are given an array of integers stones where stones[i] is the weight of the ith stone.
+We are playing a game with the stones. On each turn, we choose the heaviest two stones and smash them together. Suppose the heaviest two stones have weights x and y with x <= y. The result of this smash is:
+If x == y, both stones are destroyed, and
+If x != y, the stone of weight x is destroyed, and the stone of weight y has new weight y - x.
+At the end of the game, there is at most one stone left.
+Return the smallest possible weight of the left stone. If there are no stones left, return 0.
+ */
+
+var lastStoneWeight = function (stones) {
+  let n = stones.length;
+  if (n == 1) return stones[0];
+  if (n == 0) return 0;
+  stones.sort((a, b) => a - b);
+  if (stones[n - 1] == stones[n - 2]) {
+    return lastStoneWeight(stones.slice(0, n - 2));
+  } else {
+    let diff = stones[n - 1] - stones[n - 2];
+    return lastStoneWeight([...stones.slice(0, n - 2), diff]);
+  }
+};
+
 /* 1047. Remove all Duplicates in String
 
 You are given a string s consisting of lowercase English letters. A duplicate removal consists of choosing two adjacent and equal letters and removing them.
