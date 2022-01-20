9331bc314ccb78a920d4a2604a6a56fb0e317820
amritRaj789 <amritrazzz@gmail.com>
2022-01-20 20:44:15 +0530

    --921. Minimum Add to Make Parantheses Valid

--- leetcode_900-1000.js
+++ leetcode_900-1000.js
@@ -1,3 +1,27 @@
+/* 921. Minimum Add to Make Parantheses Valid
+
+A parentheses string is valid if and only if:
+It is the empty string,
+It can be written as AB (A concatenated with B), where A and B are valid strings, or
+It can be written as (A), where A is a valid string.
+You are given a parentheses string s. In one move, you can insert a parenthesis at any position of the string.
+For example, if s = "()))", you can insert an opening parenthesis to be "(()))" or a closing parenthesis to be "())))".
+Return the minimum number of moves required to make s valid.
+ */
+
+var minAddToMakeValid = function (s) {
+  let stack = [];
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] == ")") {
+      if (stack.length) {
+        if (stack[stack.length - 1] == "(") stack.pop();
+        else stack.push(")");
+      } else stack.push(")");
+    } else stack.push("(");
+  }
+  return stack.length;
+};
+
 /* 923. 3Sum with Multiplicity
 
 Given an integer array arr, and an integer target, return the number of tuples i, j, k such that i < j < k and arr[i] + arr[j] + arr[k] == target.
