fa5d6cc82da878fd8dc9e74ea665824330141d40
amritRaj789 <amritrazzz@gmail.com>
2022-01-20 21:19:03 +0530

    --2134. Minimum Swaps to Group All 1's together II

--- leetcode_2100-2200.js
+++ leetcode_2100-2200.js
@@ -36,3 +36,35 @@ var checkString = function (s) {
   }
   return true;
 };
+
+/* 2134. Minimum Swaps to Group All 1's Together II
+
+A swap is defined as taking two distinct positions in an array and swapping the values in them.
+A circular array is defined as an array where we consider the first element and the last element to be adjacent.
+Given a binary circular array nums, return the minimum number of swaps required to group all 1's present in the array together at any location.
+ */
+
+var minSwaps = function (nums) {
+  let numOf1 = 0;
+  for (let num of nums) {
+    if (num) numOf1++;
+  }
+  if (numOf1 <= 1) return 0;
+  let left = 0;
+  let maxInWindow = 0;
+  for (let i = 0; i < numOf1; i++) {
+    if (nums[i]) maxInWindow++;
+  }
+  let inWindow = maxInWindow;
+  for (let right = numOf1; right < nums.length; right++) {
+    if (nums[right]) inWindow++;
+    if (nums[left++]) inWindow--;
+    maxInWindow = Math.max(maxInWindow, inWindow);
+  }
+  for (let right = 0; right < numOf1; right++) {
+    if (nums[right]) inWindow++;
+    if (nums[left++]) inWindow--;
+    maxInWindow = Math.max(maxInWindow, inWindow);
+  }
+  return numOf1 - maxInWindow;
+};
