217881e68450a87555815c8077318aec8fca3854
amritRaj789 <amritrazzz@gmail.com>
2022-01-21 10:15:08 +0530

    --944. Delete Columns to Make sorted

--- leetcode_900-1000.js
+++ leetcode_900-1000.js
@@ -62,6 +62,31 @@ var threeSumMulti = function (arr, target) {
   return total % MOD;
 };
 
+/* 944. Delete Columns to Make Sorted
+
+You are given an array of n strings strs, all of the same length.
+The strings can be arranged such that there is one on each line, making a grid. For example, strs = ["abc", "bce", "cae"] can be arranged as:
+abc
+bce
+cae
+You want to delete the columns that are not sorted lexicographically. In the above example (0-indexed), columns 0 ('a', 'b', 'c') and 2 ('c', 'e', 'e') are sorted while column 1 ('b', 'c', 'a') is not, so you would delete column 1.
+Return the number of columns that you will delete
+ */
+var minDeletionSize = function (strs) {
+  let result = 0;
+  for (let i = 0; i < strs[0].length; i++) {
+    let prev = strs[0][i];
+    for (let j = 1; j < strs.length; j++) {
+      if (strs[j][i] < prev) {
+        result++;
+        break;
+      }
+      prev = strs[j][i];
+    }
+  }
+  return result;
+};
+
 /*967. Numbers with Same consecutive differences
 
 Return all non-negative integers of length n such that the absolute difference between every two consecutive digits is k.
