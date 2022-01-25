d3606eb44649c02911e94ddbcb98301bb4dc6ed2
amritRaj789 <amritrazzz@gmail.com>
2022-01-25 23:22:18 +0530

    --1431. Kids with greatest number of candies

--- leetcode_1400-1500.js
+++ leetcode_1400-1500.js
@@ -91,6 +91,25 @@ var reformat = function (s) {
   return result;
 };
 
+/* 1431. Kids with the greatest Number of Candies
+
+There are n kids with candies. You are given an integer array candies, where each candies[i] represents the number of candies the ith kid has, and an integer extraCandies, denoting the number of extra candies that you have.
+Return a boolean array result of length n, where result[i] is true if, after giving the ith kid all the extraCandies, they will have the greatest number of candies among all the kids, or false otherwise.
+Note that multiple kids can have the greatest number of candies.
+ */
+
+var kidsWithCandies = function (candies, extraCandies) {
+  let max = 0;
+  candies.forEach((candy) => {
+    max = Math.max(max, candy);
+  });
+  let result = Array(candies.length).fill(false);
+  for (let i = 0; i < candies.length; i++) {
+    if (candies[i] + extraCandies >= max) result[i] = true;
+  }
+  return result;
+};
+
 /* 1461. Check If a string contains all binary codes of size k
 
 Given a binary string s and an integer k.
