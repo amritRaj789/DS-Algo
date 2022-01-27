29a6e4215c55122e8cf55b13003675cae6bf8e51
amritRaj789 <amritrazzz@gmail.com>
2022-01-27 15:50:45 +0530

    --2150. Find All Lonely Numbers in the Array

--- leetcode_2100-2200.js
+++ leetcode_2100-2200.js
@@ -68,3 +68,39 @@ var minSwaps = function (nums) {
   }
   return numOf1 - maxInWindow;
 };
+
+/* 2150. Find All Lonely Numbers in the Array
+
+You are given an integer array nums. A number x is lonely when it appears only once, and no adjacent numbers (i.e. x + 1 and x - 1) appear in the array.
+Return all lonely numbers in nums. You may return the answer in any order. */
+
+// O(NlogN) time, O(N) space
+var findLonely = function (nums) {
+  if (nums.length == 1) return [nums[0]];
+  nums.sort((a, b) => a - b);
+  let result = [];
+  for (let i = 1; i < nums.length - 1; i++) {
+    if (nums[i - 1] < nums[i] - 1 && nums[i + 1] > nums[i] + 1)
+      result.push(nums[i]);
+  }
+  if (nums[1] > nums[0] + 1) result.push(nums[0]);
+  if (nums[nums.length - 1] > nums[nums.length - 2] + 1)
+    result.push(nums[nums.length - 1]);
+  return result;
+};
+
+// O(N) time, O(N) space
+let findLonely = function (nums) {
+  let hash = {};
+  let result = [];
+  for (let num of nums) {
+    if (!(num in hash)) hash[num] = 0;
+    hash[num]++;
+  }
+  for (let key in hash) {
+    let num = Number(key);
+    if (hash[num] == 1 && !(num - 1 in hash) && !(num + 1 in hash))
+      result.push(num);
+  }
+  return result;
+};
