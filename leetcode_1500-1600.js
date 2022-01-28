136797bb5a3a89a9e86f10021c2a0fec8ed1d436
amritRaj789 <amritrazzz@gmail.com>
2022-01-28 18:13:14 +0530

    --1559. Detect Cycle in 2D Grid

--- leetcode_1500-1600.js
+++ leetcode_1500-1600.js
@@ -35,6 +35,83 @@ var findKthBit = function (n, k) {
   return string[k - 1];
 };
 
+/* 1559. Detect Cycles in 2D Grid
+
+Given a 2D array of characters grid of size m x n, you need to find if there exists any cycle consisting of the same value in grid.
+A cycle is a path of length 4 or more in the grid that starts and ends at the same cell. From a given cell, you can move to one of the cells adjacent to it - in one of the four directions (up, down, left, or right), if it has the same value of the current cell.
+Also, you cannot move to the cell that you visited in your last move. For example, the cycle (1, 1) -> (1, 2) -> (1, 1) is invalid because from (1, 2) we visited (1, 1) which was the last visited cell.
+Return true if any cycle of the same value exists in grid, otherwise, return false.
+ */
+var containsCycle = function (grid) {
+  let m = grid.length;
+  let n = grid[0].length;
+  let visited = Array(m)
+    .fill(0)
+    .map(() => Array(n).fill(false));
+  let found = false;
+  function dfs(i, j, char, previ, prevj) {
+    if (found) return;
+    // up
+    if (i > 0) {
+      if (previ !== i - 1 && grid[i - 1][j] == char) {
+        if (visited[i - 1][j]) {
+          found = true;
+          return;
+        } else {
+          visited[i - 1][j] = true;
+          dfs(i - 1, j, char, i, j);
+        }
+      }
+    }
+    // down
+    if (i < m - 1) {
+      if (previ !== i + 1 && grid[i + 1][j] == char) {
+        if (visited[i + 1][j]) {
+          found = true;
+          return;
+        } else {
+          visited[i + 1][j] = true;
+          dfs(i + 1, j, char, i, j);
+        }
+      }
+    }
+    // left
+    if (j > 0) {
+      if (prevj !== j - 1 && grid[i][j - 1] == char) {
+        if (visited[i][j - 1]) {
+          found = true;
+          return;
+        } else {
+          visited[i][j - 1] = true;
+          dfs(i, j - 1, char, i, j);
+        }
+      }
+    }
+    // right
+    if (j < n - 1) {
+      if (prevj !== j + 1 && grid[i][j + 1] == char) {
+        if (visited[i][j + 1]) {
+          found = true;
+          return;
+        } else {
+          visited[i][j + 1] = true;
+          dfs(i, j + 1, char, i, j);
+        }
+      }
+    }
+  }
+  for (let i = 0; i < m; i++) {
+    for (let j = 0; j < n; j++) {
+      if (!visited[i][j]) {
+        visited[i][j] = true;
+        dfs(i, j, grid[i][j], i, j);
+      }
+      if (found) return found;
+    }
+  }
+  return found;
+};
+
 /* 1561. Maximum Number of Coins you Can get
 
 There are 3n piles of coins of varying size, you and your friends will take piles of coins as follows:
