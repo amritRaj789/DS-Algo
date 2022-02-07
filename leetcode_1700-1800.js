e0844ef46d8f19bdfe1782d79cbc43e035cc36ea
amritRaj789 <amritrazzz@gmail.com>
2022-02-07 15:06:45 +0530

    --1791. Find Center of Star Graph

--- leetcode_1700-1800.js
+++ leetcode_1700-1800.js
@@ -191,3 +191,15 @@ var maximumScore = function (nums, multipliers) {
   return maxScore(nums, 0, 0, length - 1);
 };
 console.log(maximumScore([-5, -3, -3, -2, 7, 1], [-10, -5, 3, 4, 6]));
+
+/* 1791. Find Center of Star Graph
+
+There is an undirected star graph consisting of n nodes labeled from 1 to n. A star graph is a graph where there is one center node and exactly n - 1 edges that connect the center node with every other node.
+You are given a 2D integer array edges where each edges[i] = [ui, vi] indicates that there is an edge between the nodes ui and vi. Return the center of the given star graph.
+ */
+
+var findCenter = function (edges) {
+  if (edges[0][0] == edges[1][0] || edges[0][0] == edges[1][1])
+    return edges[0][0];
+  return edges[0][1];
+};
