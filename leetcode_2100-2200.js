8bb3e30f9538226696b5cddf6dded934f0e4a67c
amritRaj789 <amritrazzz@gmail.com>
2022-02-19 21:04:43 -0800

    --2181. Merge Nodes in between Zeros

--- leetcode_2100-2200.js
+++ leetcode_2100-2200.js
@@ -104,3 +104,28 @@ let findLonely = function (nums) {
   }
   return result;
 };
+
+/* 2181. Merge Nodes in Between Zeroes
+
+You are given the head of a linked list, which contains a series of integers separated by 0's. The beginning and end of the linked list will have Node.val == 0.
+For every two consecutive 0's, merge all the nodes lying in between them into a single node whose value is the sum of all the merged nodes. The modified list should not contain any 0's.
+Return the head of the modified linked list.
+ */
+
+let mergeNodes = function (head) {
+  let p1 = head;
+  let copyp1 = head;
+  let sum = 0;
+  head = head.next;
+  while (head) {
+    if (head.val == 0) {
+      p1 = p1.next;
+      p1.val = sum;
+      sum = 0;
+    }
+    sum += head.val;
+    head = head.next;
+  }
+  p1.next = null;
+  return copyp1.next;
+};
