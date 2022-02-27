b3a6afe3506507bef032c03c3382c99f6e03ef3b
amritRaj789 <amritrazzz@gmail.com>
2022-02-26 23:48:42 -0800

    --2187. Minimum Time to Complete Trips

--- leetcode_2100-2200.js
+++ leetcode_2100-2200.js
@@ -249,3 +249,32 @@ var repeatLimitedString = function (s, repeatLimit) {
   }
   return result;
 };
+
+/* 2187. Minimum Time to Complete Trips
+
+You are given an array time where time[i] denotes the time taken by the ith bus to complete one trip.
+Each bus can make multiple trips successively; that is, the next trip can start immediately after completing the current trip. Also, each bus operates independently; that is, the trips of one bus do not influence the trips of any other bus.
+You are also given an integer totalTrips, which denotes the number of trips all buses should make in total. Return the minimum time required for all buses to complete at least totalTrips trips.
+ */
+
+let minimumTime = function (time, totalTrips) {
+  let low = 1;
+  let high = Number.MAX_SAFE_INTEGER;
+  let result = 0;
+  while (low <= high) {
+    let mid = Math.floor((high + low) / 2);
+    if (check(mid)) {
+      result = mid;
+      high = mid - 1;
+    } else low = mid + 1;
+  }
+
+  function check(t) {
+    let trips = 0;
+    for (let i = 0; i < time.length; i++) {
+      trips += Math.floor(t / time[i]);
+    }
+    return trips >= totalTrips;
+  }
+  return result;
+};
