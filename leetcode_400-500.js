f3c213cb7729bba5633397cd61a08d847fb57638
amritRaj789 <amritrazzz@gmail.com>
2022-03-03 11:48:47 -0800

    --413. Arithmetic Slices

--- leetcode_400-500.js
+++ leetcode_400-500.js
@@ -120,6 +120,26 @@ let fizzBuzz = function (n) {
   return array;
 };
 
+/* 413. Arithmetic Slices
+
+An integer array is called arithmetic if it consists of at least three elements and if the difference between any two consecutive elements is the same.
+For example, [1,3,5,7,9], [7,7,7,7], and [3,-1,-5,-9] are arithmetic sequences.
+Given an integer array nums, return the number of arithmetic subarrays of nums.
+ */
+
+var numberOfArithmeticSlices = function (nums) {
+  if (nums.length <= 2) return 0;
+  let count = 0;
+  let dp = Array(nums.length).fill(0);
+  for (let i = 2; i < nums.length; i++) {
+    if (nums[i] - nums[i - 1] == nums[i - 1] - nums[i - 2]) {
+      dp[i] = dp[i - 1] + 1;
+      count += dp[i];
+    }
+  }
+  return count;
+};
+
 /* 415. Add Strings
 
 Given two non-negative integers, num1 and num2 represented as string, return the sum of num1 and num2 as a string.
