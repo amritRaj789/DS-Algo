5b44ab166e07d5c6c9cf2a5b03811f78708aa227
amritRaj789 <amritrazzz@gmail.com>
2022-03-07 22:41:29 -0800

    --977. Squares of a Sorted Array

--- leetcode_900-1000.js
+++ leetcode_900-1000.js
@@ -247,6 +247,28 @@ var kClosest = function (points, k) {
   return finalResult;
 };
 
+/* 977. Squares of a Sorted Array
+
+Given an integer array nums sorted in non-decreasing order, return an array of the squares of each number sorted in non-decreasing order.
+ */
+
+var sortedSquares = function (nums) {
+  let result = Array(nums.length).fill(0);
+  let i = nums.length - 1;
+  let left = 0;
+  let right = nums.length - 1;
+  while (left <= right) {
+    if (Math.abs(nums[left]) >= Math.abs(nums[right])) {
+      result[i--] = nums[left] ** 2;
+      left++;
+    } else {
+      result[i--] = nums[right] ** 2;
+      right--;
+    }
+  }
+  return result;
+};
+
 /*
 978. Longest Turbulent SubArray
 
