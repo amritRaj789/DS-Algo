83df22330918ffe8a1a6ec98b0d06322d0d6b062
amritRaj789 <amritrazzz@gmail.com>
2023-01-07 16:13:33 -0800

    ---m

--- leetcode_300_400.js
+++ leetcode_300_400.js
@@ -54,6 +54,24 @@ var removeDuplicateLetters = function (s) {
   return stack.join("");
 };
 
+/* 322. Coin Change
+You are given an integer array coins representing coins of different denominations and an integer amount representing a total amount of money.
+Return the fewest number of coins that you need to make up that amount. If that amount of money cannot be made up by any combination of the coins, return -1.
+You may assume that you have an infinite number of each kind of coin. */
+
+var coinChange = function (coins, amount) {
+  let dp = Array(amount + 1).fill(+Infinity);
+  dp[0] = 0;
+  coins.sort((a, b) => a - b);
+  for (let i = 1; i < amount + 1; i++) {
+    for (let j = 0; j < coins.length; j++) {
+      if (coins[j] <= i) dp[i] = Math.min(dp[i], 1 + dp[i - coins[j]]);
+      else break;
+    }
+  }
+  return dp[amount] == +Infinity ? -1 : dp[amount];
+};
+
 /* 328. Odd Even Linked List
 
 Given the head of a singly linked list, group all the nodes with odd indices together followed by the nodes with even indices, and return the reordered list.
